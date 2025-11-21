5f2d2ae144f84d9fda5e946475e43109b2f7ee93
Svaishnavi2001 <svaishnavi823@gmail.com>
2025-11-21 17:46:19 +0530

    Add initial project structure and implement core classes for OOP concepts
    - Created .gitignore to exclude IDE and build files.
    - Added Address, Person, Student, and Teacher classes to represent OOP concepts.
    - Implemented methods for managing attributes and relationships.
    - Included example main classes to demonstrate functionality.

---
+++ src/problems/oops/constructor_chaining/Car.java
@@ -0,0 +1,35 @@
+package problems.oops.constructor_chaining;
+
+public class Car {
+    String brand;
+    double price;
+    int year;
+
+    public Car(){
+        // calling 1 argument constructor
+        this("unknown");
+        System.out.println("Default constructor ");
+
+    }
+    public Car(String brand){
+        //calling 2 argument constructor
+        this("Honda", 2022);
+
+        System.out.println("1 argument constructor: "+brand);
+    }
+
+    public Car(String brand, int year){
+        // calling 3 argument constructor
+        this("Honda",45000,2002);
+        System.out.println("2 argument constructor: "+ brand+","+price);
+    }
+
+    public Car(String brand ,double price, int year){
+       this.brand=brand;
+       this.price=price;
+       this.year=year;
+
+        System.out.println("3 argument constructor: "+ brand+","+price+","+year);
+
+    }
+}
